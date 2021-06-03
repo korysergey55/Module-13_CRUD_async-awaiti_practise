652a6148f5806788f1c389dbb82fba28a3fb0d38
Serhiy Korytskyy <korysergey55@gmail.com>
2021-06-03 16:24:02 +0300

    ref

--- src/js/users.js
+++ src/js/users.js
@@ -1,5 +1,5 @@
 import { Modal, Toast } from 'bootstrap';
-import * as api from './services/crud';
+import * as api from './crud';
 import getFormData from './services/getFormData';
 import findNameErrors from './services/validateUser';
 import createUsersMarkup from '../templates/users.hbs';
@@ -19,14 +19,14 @@ const myToast = new Toast(refs.toast, { delay: 6000 });
 
 // ********************* READ  ********************* //
 
-// Promise
+// ********************* Promise *********************
 
-// const renderAllUsers = () =>
-//   api.getUsers().then(users => {
-//     refs.usersList.innerHTML = createUsersMarkup(users);
-//   });
+const renderAllUsers = () =>
+  api.getUsers().then(users => {
+    refs.usersList.innerHTML = createUsersMarkup(users);
+  });
 
-// async / await
+// *********************async / await*********************
 
 const renderAllUsers = async () => {
   const users = await api.getUsers();
@@ -36,11 +36,30 @@ const renderAllUsers = async () => {
 renderAllUsers();
 
 // ********************* CREATE  ********************* //
-
 refs.form.addEventListener('submit', saveUser);
 
-// async / await
+// ********************* Promise *********************
+function saveUser(e) {
+  e.preventDefault();
 
+  const form = e.currentTarget;
+  const newUser = getFormData(form.elements);
+
+  const nameError = findNameErrors(newUser);
+  if (nameError) {
+    refs.toastText.textContent = nameError;
+    myToast.show();
+    return;
+  }
+
+  newUser.icon = 'https://cdn4.iconfinder.com/data/icons/pretty_office_3/256/Add-Male-User.png';
+
+  api.saveUser(newUser).then(renderAllUsers);
+
+  form.reset();
+}
+
+// ********************* async / await *********************
 async function saveUser(e) {
   e.preventDefault();
 
@@ -59,39 +78,24 @@ async function saveUser(e) {
   await api.saveUser(newUser);
   renderAllUsers();
 
-  // api.saveUser(newUser).then(renderAllUsers);
-
   form.reset();
 }
 
-// Promise
-
-// function saveUser(e) {
-//   e.preventDefault();
-
-//   const form = e.currentTarget;
-//   const newUser = getFormData(form.elements);
-
-//   const nameError = findNameErrors(newUser);
-//   if (nameError) {
-//     refs.toastText.textContent = nameError;
-//     myToast.show();
-//     return;
-//   }
-
-//   newUser.icon = 'https://cdn4.iconfinder.com/data/icons/pretty_office_3/256/Add-Male-User.png';
-
-//   api.saveUser(newUser).then(renderAllUsers);
-
-//   form.reset();
-// }
-
-// // ********************* UPDATE / DELETE  ********************* //
+// ********************* UPDATE / DELETE  ********************* //
 
 refs.usersList.addEventListener('click', handleListClick);
 
-// async / await
+//********************* Promise *********************
+function handleListClick(e) {
+  if (e.target.dataset.action === 'delete') {
+    api.deleteUser(e.target.dataset.id).then(renderAllUsers);
+  }
 
+  if (e.target.dataset.action === 'edit') {
+    handleEditUser(e.target.dataset.id);
+  }
+}
+//********************* async / await *********************
 async function handleListClick(e) {
   if (e.target.dataset.action === 'delete') {
     await api.deleteUser(e.target.dataset.id);
@@ -102,21 +106,43 @@ async function handleListClick(e) {
     handleEditUser(e.target.dataset.id);
   }
 }
+//********************* Promise *********************
+function handleEditUser(id) {
+  const inputs = refs.modalForm.elements;
 
-// Promise
+  api.getUserById(id).then(user => {
+    // Object.entries(user).forEach(([key, value]) => {
+    //   if (inputs.hasOwnProperty(key)) {
+    //     inputs[key].value = value;
+    //   }
+    // });
 
-// function handleListClick(e) {
-//   if (e.target.dataset.action === 'delete') {
-//     api.deleteUser(e.target.dataset.id).then(renderAllUsers);
-//   }
+    Object.keys(user).forEach(key => {
+      if (inputs.hasOwnProperty(key)) {
+        inputs[key].value = user[key];
+      }
+    });
 
-//   if (e.target.dataset.action === 'edit') {
-//     handleEditUser(e.target.dataset.id);
-//   }
-// }
+    refs.updateBtn.addEventListener('click', saveChanges);
 
-// async / await
+    function saveChanges() {
+      const updatedUser = getFormData(inputs);
 
+      const nameError = findNameErrors(updatedUser);
+      if (nameError) {
+        refs.toastText.textContent = nameError;
+        myToast.show();
+        return;
+      }
+
+      api.editUser(id, updatedUser).then(renderAllUsers);
+      myModal.hide();
+
+      refs.updateBtn.removeEventListener('click', saveChanges);
+    }
+  });
+}
+//********************* async / await *********************
 async function handleEditUser(id) {
   const inputs = refs.modalForm.elements;
 
@@ -146,78 +172,6 @@ async function handleEditUser(id) {
     myModal.hide();
     refs.updateBtn.removeEventListener('click', saveChanges);
   }
-
-  // Promise
-
-  // api.getUserById(id).then(user => {
-  // Object.entries(user).forEach(([key, value]) => {
-  //   if (inputs.hasOwnProperty(key)) {
-  //     inputs[key].value = value;
-  //   }
-  // });
-
-  // Object.keys(user).forEach(key => {
-  //   if (inputs.hasOwnProperty(key)) {
-  //     inputs[key].value = user[key];
-  //   }
-  // });
-
-  // refs.updateBtn.addEventListener('click', saveChanges);
-
-  // function saveChanges() {
-  //   const updatedUser = getFormData(inputs);
-
-  //   const nameError = findNameErrors(updatedUser);
-  //   if (nameError) {
-  //     refs.toastText.textContent = nameError;
-  //     myToast.show();
-  //     return;
-  //   }
-
-  //   api.editUser(id, updatedUser).then(renderAllUsers);
-  //   myModal.hide();
-
-  //   refs.updateBtn.removeEventListener('click', saveChanges);
-  // }
-  // });
-}
-
-// Promise
-
-// function handleEditUser(id) {
-//   const inputs = refs.modalForm.elements;
-
-//   api.getUserById(id).then(user => {
-//     // Object.entries(user).forEach(([key, value]) => {
-//     //   if (inputs.hasOwnProperty(key)) {
-//     //     inputs[key].value = value;
-//     //   }
-//     // });
-
-//     Object.keys(user).forEach(key => {
-//       if (inputs.hasOwnProperty(key)) {
-//         inputs[key].value = user[key];
-//       }
-//     });
-
-//     refs.updateBtn.addEventListener('click', saveChanges);
-
-//     function saveChanges() {
-//       const updatedUser = getFormData(inputs);
-
-//       const nameError = findNameErrors(updatedUser);
-//       if (nameError) {
-//         refs.toastText.textContent = nameError;
-//         myToast.show();
-//         return;
-//       }
-
-//       api.editUser(id, updatedUser).then(renderAllUsers);
-//       myModal.hide();
-
-//       refs.updateBtn.removeEventListener('click', saveChanges);
-//     }
-//   });
-// }
+}  
 
 refs.modal.addEventListener('hidden.bs.modal', () => refs.modalForm.reset());
